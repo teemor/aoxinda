76f984eefcbe720bc8683080b2e8bfc417c27d22
teemo <1130477946@qq.com>
2018-12-13 10:14:59 +0800

    添加爱车

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,6 @@ const app = getApp()
 Page({
   data: {
     background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
-    indicatorDots: true,
     vertical: false,
     autoplay: false,
     circular: false,
@@ -13,5 +12,11 @@ Page({
     nextMargin: 0
   },
   onLoad: function () {
+  },
+  // 添加我的爱车
+  addCar: function () {
+    wx.navigateTo({
+      url:'../../pages/add_car/index'
+    })
   }
 })
