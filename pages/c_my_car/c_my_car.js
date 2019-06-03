f06df13c607c2692a1d8b559379e1c1baecacf56
yd <2890337138@qq.com>
2019-06-03 18:14:08 +0800

    服务更换车辆页面

---
+++ pages/c_my_car/c_my_car.js
@@ -0,0 +1,67 @@
+import { Technician } from '../../common/api/api'
+const request = new Technician
+import find_car from '../../mixin/find_car'
+
+Page({
+  mixins: [find_car],
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: ['', '']
+  },
+  /**
+   * 添加爱车
+   */
+  addCar: function () {
+    wx.navigateTo({
+      url: `../add_car_mes/index`
+    })
+  },
+  /**
+   * 跳转车保养
+   */
+  toFWcar: function (e) {
+    console.log(e)
+    wx.getStorage({
+      key: 'userPhone',
+      success: function (res) {
+        request.updateDef({
+          userTel: res.data,
+          carId: e.currentTarget.dataset.carid
+        }).then(res => {
+          if (res.code == '200') {
+            wx.navigateTo({
+              url: `../c_upkeep_car/c_upkeep_car`
+            })
+          } else {
+            wx.showToast({
+              title: '设置失败',
+              icon: 'warning',
+              duration: 1500
+            })
+          }
+        })
+
+      },
+    })
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  onShow: function () {
+    this.findCarList()
+  }
+})
