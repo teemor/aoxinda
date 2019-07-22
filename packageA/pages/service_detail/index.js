8c233b38096ac87e8820f34ce6399baac8f7be0d
ug <1>
2019-07-22 11:10:59 +0800

    单次服务查看评论列表

--- packageA/pages/service_detail/index.js
+++ packageA/pages/service_detail/index.js
@@ -9,7 +9,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    commentList: [], //评论
+    load: true,
+    loading: false,//加载动画的显示
+    commentForm: {
+      "pageIndex": 1,
+      "pageSize": 10,
+      "activity_id": ""
+    },
+    tabIndex: 0   //当前tabs页签下标
   },
 
   /**
@@ -26,6 +34,9 @@ Page({
       } else if (model.serType[0].name == '月月卡') {
         isNot = 4
       }
+      this.setData({
+        ["commentForm.activity_id"]: model.serType[0].actId
+      })
       request.findPayType({
         actId: model.serType[0].actId,
         actCardType: isNot,
@@ -39,6 +50,25 @@ Page({
         console.log(res)
       })
     }
+  },
+
+  //洗车评价
+  washCarComment: function (model) {
+    request.selectWashCarComment(model).then(res => {
+      if (res.status == '200') {
+        this.setData({
+          commentList: res.data.consumeCommentList
+        })
+      }
+    })
+  },
+  //tab点击事件
+  tabClick(event) {
+    this.setData({
+      tabIndex: event.detail.index,
+      ["commentForm.pageIndex"]: 1
+    });
+    this.washCarComment(this.data.commentForm);
   },
 
   /**
@@ -80,8 +110,35 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
-
+  onReachBottom: function () {
+    var that = this;
+    if (that.data.load && that.data.tabIndex == 2) {//全局标志位，方式请求未响应时多次触发
+      that.setData({
+        ["commentForm.pageIndex"]: that.data.commentForm.pageIndex * 1 + 1,
+        load: false,
+        loading: true//加载动画的显示
+      })
+      request.selectWashCarComment(that.data.commentForm).then(res => {
+        if (res.status == '200' && res.data.consumeCommentList.length > 0) {
+          var content = that.data.commentList.concat(res.data.consumeCommentList)//将返回结果放入content
+          that.setData({
+            commentList: content,
+            load: true,
+            loading: false
+          })
+        } else {
+          that.setData({
+            loading: false,
+            load: true
+          })
+          wx.showToast({
+            title: '没有更多数据',
+            icon: 'none',
+            duration: 2000,
+          })
+        }
+      })
+    }
   },
 
   /**
