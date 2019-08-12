744993fd7f83f6b9a045ac959efd5d32348c1ced
teemor <1130477946@qq.com>
2019-08-12 17:05:26 +0800

    bug

--- components/clean_order_list/clean_order_list.js
+++ components/clean_order_list/clean_order_list.js
@@ -26,12 +26,7 @@ Component({
      * 进入详情
      */
     orderDetail: function () {
-      console.log(this.data.goodsList, 'hhhhh')
-      if (this.data.goodsList.wechatRefundNo === undefined) {
         this.triggerEvent('orderDetail', { id: this.data.goodsList.id })
-       } else {
-        this.triggerEvent('orderDetail', { wechatRefundNo: this.data.goodsList.wechatRefundNo })
-      }
     },
     /**
      * 查看服务单详情
