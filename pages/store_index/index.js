b0f078fc57eed8b435e1c5a5d4ed65c279cc3032
teemor <1130477946@qq.com>
2019-08-21 15:31:09 +0800

    bug

--- pages/store_index/index.js
+++ pages/store_index/index.js
@@ -191,7 +191,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.findShopList(app.carType,'','',app.actCarCode,'')
+    this.findShopList(app.carType,'','',app.actCarCode,'','')
     console.log(app.address,'dizhi ')
     this.setData({
       location:app.address
